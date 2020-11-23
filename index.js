a115054850e5dc4962f4526aa81434a857559dfe
Julian Vos <post@julianvos.nl>
2020-11-23 16:06:54 +0000

    Added test data

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const https = require('https')
 const querystring = require('querystring')
 
-const orderId = ''
-const postCode = ''
+require('dotenv').config()
+
+const orderId = process.env.ORDERID
+const postCode = process.env.POSTCODE
 
 const postData = querystring.stringify({
     'ajax': 'lookup',
@@ -23,6 +25,8 @@ const postOptions = {
     method: 'POST',
     headers: postHeaders
 }
+
+const data = '<div style=\'display:inline-block; width:100px\'><b>Order:</b></div>        <div style=\'display:inline-block;\'>ME211120000721</div><br><div style=\'display:inline-block; width:100px\'><b>Artikelnr</b>:</div>    <div style=\'display:inline-block;\'>294824</div><br><div style=\'display:inline-block; width:100px\'><b>Artikel</b>:</div>      <div style=\'display:inline-block;\'>Processor AMD Ryzen 5 5600X</div><br><div style=\'display:inline-block; width:100px\'><b>Jouw positie:</b></div> <div style=\'display:inline-block;\'>443 van de 493</div><br><br>'
 
 const req = https.request(postOptions, (res) => {
     console.log(res.statusCode, '\n')
